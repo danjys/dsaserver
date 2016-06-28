2066710881c72c9fabbd114d2ec3c95a10cd91d8
Dhananjaya Somanna <danjys@gmail.com>
2016-06-28 11:56:44 -0400

    Schema Definition Complete

--- models/base-shape-schema.js
+++ models/base-shape-schema.js
@@ -7,6 +7,7 @@ var mongoose = restful.mongoose;
 var Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
+// Base Shape
 var baseSchema = new Schema({
 	label: {
     value : String,
@@ -18,14 +19,68 @@ var baseSchema = new Schema({
   lineWidth: Number,
 }, {collection: 'baseshape', discriminatorKey : '_type'});
 
+// Point Schema
 var pointShapeSchema = baseSchema.extend({
 	center: { x: Number, y: Number, z: Number}
 });
 
+// Arrow Schema
+var arrowShapeSchema = baseSchema.extend({
+	points: {
+    head: { x: Number, y: Number, z: Number},
+    tail: { x: Number, y: Number, z: Number}
+  },
+  fillColor: String
+});
+
+// Circle Schema
+var circleShapeSchema = baseSchema.extend({
+	center: { x: Number, y: Number, z: Number},
+  radius: Number,
+  fillColor: String
+});
+
+// Polyline Schema
+var polylineShapeSchema = baseSchema.extend({
+	points: [
+    { x: Number, y: Number, z: Number}
+  ],
+  closed: Boolean,
+  fillColor: String
+});
+
+// Rectangle Schema
+var rectangleShapeSchema = baseSchema.extend({
+	center: { x: Number, y: Number, z: Number},
+  width: Number,
+  height: Number,
+  rotation: Number,
+  normal: { x: Number, y: Number, z: Number},
+  fillColor: String
+});
+
+// Rectangle Grid Schema
+var rectangleGridShapeSchema = rectangleShapeSchema.extend({
+	widthSubdivisions: Number,
+  heightSubdivisions: Number
+});
+
+
 var BaseShape = restful.model('BaseShape', baseSchema);
 var Point = restful.model('point', pointShapeSchema);
+var Arrow = restful.model('arrow', arrowShapeSchema);
+var Circle = restful.model('circle', circleShapeSchema);
+var Polyline = restful.model('polyline', polylineShapeSchema);
+var Rectangle = restful.model('rectangle', rectangleShapeSchema);
+var RectangleGrid = restful.model('rectangle_grid', rectangleGridShapeSchema);
+
 //Retrun model
 module.exports = {
   BaseShape: BaseShape,
-  Point: Point
+  Point: Point,
+  Arrow: Arrow,
+  Circle: Circle,
+  Polyline: Polyline,
+  Rectangle: Rectangle,
+  RectangleGrid: RectangleGrid
 };
--- routes/api.js
+++ routes/api.js
@@ -11,8 +11,26 @@ Annotation.methods(['get', 'put', 'post', 'delete']);
 Annotation.register(router, '/annotations');
 
 // Routes
-BaseShape.Point.methods(['get', 'put', 'post', 'delete']);
-BaseShape.Point.register(router, '/points');
+BaseShape.BaseShape.methods(['get', 'delete']);
+BaseShape.BaseShape.register(router, '/shapes');
+
+BaseShape.Point.methods(['put', 'post']);
+BaseShape.Point.register(router, '/point');
+
+BaseShape.Arrow.methods(['put', 'post']);
+BaseShape.Arrow.register(router, '/arrow');
+
+BaseShape.Circle.methods(['put', 'post']);
+BaseShape.Circle.register(router, '/circle');
+
+BaseShape.Polyline.methods(['put', 'post']);
+BaseShape.Polyline.register(router, '/polyline');
+
+BaseShape.Rectangle.methods(['put', 'post']);
+BaseShape.Rectangle.register(router, '/rectangle');
+
+BaseShape.RectangleGrid.methods(['put', 'post']);
+BaseShape.RectangleGrid.register(router, '/rectanglegrid');
 
 // Return router
 module.exports = router;
